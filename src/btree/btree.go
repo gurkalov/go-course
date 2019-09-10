ce09e77818f49513a42cdb4ddd053b9932033afb
Dmitry Gurkalov <gurkalov.dev@gmail.com>
2019-09-10 10:26:54 +0300

    All delete cases for Btree

--- src/btree/btree.go
+++ src/btree/btree.go
@@ -81,6 +81,12 @@ func (T *BTree) SplitChild (x *Node, i uint) {
 	}
 
 	x.Key[i] = y.Key[T.t]
+	// clear y part that moved to z
+	for j := y.N + 1; j < 2 * T.t; j++ {
+		y.Key[j] = ""
+		y.C[j + 1] = nil
+	}
+
 	x.N = x.N + 1
 }
 
@@ -112,21 +118,196 @@ func (T *BTree) Search (x *Node, k string) (*Node, uint) {
 	} else if x.Leaf {
 		return nil, 0
 	} else {
+		if x.C[i] == nil {
+			return nil, 0
+		}
 		return T.Search(x.C[i], k)
 	}
 }
 
-func (T *BTree) Delete (x *Node, k string) {
+func (x *Node) RemoveFromLeaf (i uint) {
+	for j := i; j < x.N; j++ {
+		x.Key[j] = x.Key[j + 1]
+	}
+	x.Key[x.N] = ""
+	for j := i; j <= x.N; j++ {
+		x.C[j] = x.C[j + 1]
+	}
+	x.C[x.N + 1] = nil
+	x.SetN(x.N - 1)
+}
+
+func (x *Node) MergePair (p, y *Node) {
+	x.Key[x.N + 1] = p.Key[1]
+	for j := uint(1); j <= y.N; j++ {
+		x.Key[x.N + 1 + j] = y.Key[j]
+	}
+
+	for j := uint(1); j <= y.N + 1; j++ {
+		x.C[x.N + 1 + j] = y.C[j]
+	}
+
+	x.SetN(x.N + y.N + 1)
+	y = nil
+}
+
+func (x *Node) SetN (n uint) {
+	start := uint(n + 2)
+	if n == 0 {
+		start = 1
+	}
+	for j := start; j < uint(len(x.C)); j++ {
+		x.C[j] = nil
+	}
+	x.N = n
+}
+
+func (x *Node) MergeSibling (n, y *Node, i uint) {
+	x.Key[x.N + 1] = n.Key[i]
+	for k := uint(1); k <= y.N; k++ {
+		y.Key[x.N + 1 + k] = y.Key[k]
+	}
+
+	for k := uint(1); k <= x.N; k++ {
+		y.Key[k] = x.Key[k]
+	}
+	y.Key[x.N + 1] = n.Key[i]
+	y.SetN(x.N + 1 + y.N)
+	n.RemoveFromLeaf(i)
+}
+
+func (T *BTree) NeedMergeAll (n *Node) bool {
+	if n == nil {
+		return false
+	}
+	if n.N == 1 && n.C[1] != nil && n.C[1].N == T.t - 1 && n.C[2] != nil && n.C[2].N == T.t - 1 {
+		return true
+	}
+	return false
+}
+
+func (T *BTree) TryUp (n *Node, i uint) bool {
+	if n.C[i].N > T.t - 1 {
+		n.Key[i] = n.C[i].Key[n.C[i].N]
+		n.C[i].RemoveFromLeaf(n.C[i].N)
+		return true
+	}
+
+	if n.C[i + 1].N > T.t - 1 {
+		n.Key[i] = n.C[i + 1].Key[1]
+		n.C[i + 1].RemoveFromLeaf(1)
+		return true
+	}
+
+	return false
+}
+
+func (T *BTree) TryTurn (n *Node, i uint, key string) bool {
+	if n.C[i] == nil || n.C[i].N > T.t - 1 {
+		return false
+	}
+
+	j := uint(1)
+	for j <= n.C[i].N && key > n.C[i].Key[j] {
+		j = j + 1
+	}
+
+	if n.C[i].Key[j] == key {
+		if i > 1 && n.C[i - 1] != nil && n.C[i - 1].N > T.t - 1 {
+			n.C[i].Key[j] = n.Key[i - 1]
+			n.Key[i - 1] = n.C[i - 1].Key[n.C[i - 1].N]
+			n.C[i - 1].RemoveFromLeaf(n.C[i - 1].N)
+			return true
+		} else if n.C[i + 1] != nil && n.C[i + 1].N > T.t - 1 {
+			n.C[i].RemoveFromLeaf(j)
+			n.C[i].Key[n.C[i].N + 1] = n.Key[i]
+			n.C[i].SetN(n.C[i].N + 1)
+			n.Key[i] = n.C[i + 1].Key[1]
+			n.C[i + 1].RemoveFromLeaf(1)
+			return true
+		}
+	}
+
+	return false
+}
+
+func (T *BTree) TryMerge (n *Node, i uint, key string) bool {
+	if (n.C[i] != nil && n.C[i].N > T.t - 1) || (i <= n.N && n.C[i + 1] != nil && n.C[i + 1].N > T.t - 1) || (i > 1 && n.C[i - 1] != nil && n.C[i - 1].N > T.t - 1) {
+		return false
+	}
+
+	if i <= n.N {
+		n.C[i].Key[n.C[i].N+1] = n.Key[i]
+		n.C[i].C[n.C[i].N+2] = n.C[i + 1].C[1]
+		for j := uint(1); j <= n.C[i+1].N; j++ {
+			n.C[i].Key[n.C[i].N+1+j] = n.C[i+1].Key[j]
+			n.C[i].C[n.C[i].N+2+j] = n.C[i+1].C[j + 1]
+		}
+
+		n.C[i].SetN(n.C[i].N + 1 + n.C[i+1].N)
+		j := uint(i)
+		for ; j < n.N; j++ {
+			n.Key[j] = n.Key[j+1]
+			n.C[j+1] = n.C[j+2]
+		}
+		n.Key[j] = ""
+		n.SetN(n.N - 1)
+		T.Delete(n.C[i], key)
+	} else {
+		n.C[i - 1].Key[n.C[i].N + 1] = n.Key[i - 1]
+		for j := uint(1); j <= n.C[i].N; j++ {
+			n.C[i - 1].Key[n.C[i].N + 1 + j] = n.C[i].Key[j]
+		}
+
+		n.C[i - 1].SetN(n.C[i-1].N + 1 + n.C[i].N)
+		j := uint(i)
+		for ; j < n.N; j++ {
+			n.Key[j] = n.Key[j+1]
+			n.C[j+1] = n.C[j+2]
+		}
+		n.Key[j - 1] = ""
+		n.SetN(n.N - 1)
+
+		T.Delete(n.C[i - 1], key)
+	}
+	return true
+}
+
+func (T *BTree) Delete (n *Node, key string) {
 	i := uint(1)
-	for i <= x.N && k > x.Key[i] {
+	for i <= n.N && key > n.Key[i] {
 		i = i + 1
 	}
-	if i <= x.N && k == x.Key[i] {
-		x.Key[i] = ""
-		return
-	} else if x.Leaf {
-		return
+
+	if !n.Leaf {
+		if T.NeedMergeAll(n) {
+			n.C[1].MergePair(n, n.C[2])
+			if n == T.root {
+				T.root = n.C[1]
+			}
+			T.Delete(n.C[1], key)
+		} else if T.NeedMergeAll(n.C[i]) {
+			n.C[i].C[1].MergePair(n.C[i], n.C[i].C[2])
+			n.C[i] = n.C[i].C[1]
+
+			T.Delete(n.C[i], key)
+		} else if i <= n.N && key == n.Key[i] {
+			if !T.TryUp(n, i) {
+				n.C[i].MergeSibling(n, n.C[i + 1], i)
+				T.Delete(n.C[i], key)
+			}
+		} else {
+			if T.TryMerge(n, i, key) {
+				return
+			} else if T.TryTurn(n, i, key) {
+				return
+			}
+
+			T.Delete(n.C[i], key)
+		}
 	} else {
-		T.Delete(x.C[i], k)
+		if i <= n.N && key == n.Key[i] {
+			n.RemoveFromLeaf(i)
+		}
 	}
 }
--- src/btree/btree_test.go
+++ src/btree/btree_test.go
@@ -2,6 +2,8 @@ package btree
 
 import (
 	"os"
+	"reflect"
+	"strconv"
 	"testing"
 )
 
@@ -55,7 +57,7 @@ func TestSearch2(t *testing.T) {
 
 func TestDelete(t *testing.T) {
 	nodes := []string{"A", "D", "F", "L", "N", "P", "H"}
-	deleteNodes := []string{"A", "F", "N", "P", "H"}
+	deleteNodes := []string{"A", "F", "N", "P", "H", "L", "D"}
 
 	rootTree := &BTree{nil, 2}
 	rootTree.Create()
@@ -82,6 +84,453 @@ func TestDelete(t *testing.T) {
 			t.Errorf("Found deleted element: %s", node)
 		}
 	}
+}
 
+func TestDeleteE(t *testing.T) {
+	nodes := []string{"P", "C", "T", "G", "M", "X", "A", "B", "D", "Q", "R", "U", "V", "E", "F", "J", "K", "Y", "Z", "N", "O"}
+	deleteNodes := []string{"E"}
 
+	rootTree := &BTree{nil, 3}
+	rootTree.Create()
+
+	for _, node := range nodes {
+		rootTree.Insert(node)
+	}
+
+	for _, node := range deleteNodes {
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:6], []string{"C", "G", "M", "R", "V"}) {
+		t.Error("Error, expected: [C G M R V]")
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:3], []string{"D", "F"}) {
+		t.Error("Error, expected: [D F]")
+	}
+}
+
+func TestDeleteD(t *testing.T) {
+	nodes := []string{"P", "C", "T", "G", "M", "X", "A", "B", "D", "Q", "R", "U", "V", "E", "F", "J", "K", "Y", "Z", "N", "O"}
+	deleteNodes := []string{"D"}
+
+	rootTree := &BTree{nil, 3}
+	rootTree.Create()
+
+	for _, node := range nodes {
+		rootTree.Insert(node)
+	}
+
+	for _, node := range deleteNodes {
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:3], []string{"C", "G"}) {
+		t.Error("Error, expected: [C G]")
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:3], []string{"E", "F"}) {
+		t.Error("Error, expected: [E F]")
+	}
+}
+
+func TestDeleteL(t *testing.T) {
+	nodes := []string{"A", "B", "C", "D", "E", "F", "G", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"}
+	deleteNodes := []string{"L"}
+
+	rootTree := &BTree{nil, 3}
+	rootTree.Create()
+
+	for _, node := range nodes {
+		rootTree.Insert(node)
+	}
+
+	for _, node := range deleteNodes {
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:3], []string{"P", "S"}) {
+		t.Error("Error, expected: [P S]")
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].C[1].Key[1:5], []string{"K", "M", "N", "O"}) {
+		t.Error("Error, expected: [K M N O]")
+	}
+}
+
+func TestDeleteK(t *testing.T) {
+	nodes := []string{"A", "B", "C", "D", "E", "F", "G", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"}
+	deleteNodes := []string{"K"}
+
+	rootTree := &BTree{nil, 3}
+	rootTree.Create()
+
+	for _, node := range nodes {
+		rootTree.Insert(node)
+	}
+
+	for _, node := range deleteNodes {
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:3], []string{"P", "S"}) {
+		t.Error("Error, expected: [P S]")
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].C[1].Key[1:5], []string{"L", "M", "N", "O"}) {
+		t.Error("Error, expected: [L M N O]")
+	}
+}
+
+func TestDeleteX(t *testing.T) {
+	nodes := []string{"P", "C", "G", "T", "X", "A", "B", "E", "F", "J", "K", "L", "N", "O", "Q", "R", "U", "V", "Y", "Z", "H", "D", "I", "W"}
+	deleteNodes := []string{"X"}
+
+	rootTree := &BTree{nil, 3}
+	rootTree.Create()
+
+	for _, node := range nodes {
+		rootTree.Insert(node)
+	}
+
+	for _, node := range deleteNodes {
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:4], []string{"P", "T", "W"}) {
+		t.Error("Error, expected: [P T W]")
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].C[3].Key[1:3], []string{"U", "V"}) {
+		t.Error("Error, expected: [U V]")
+	}
+}
+
+func TestDeleteT(t *testing.T) {
+	nodes := []string{"P", "C", "G", "T", "X", "A", "B", "E", "F", "J", "K", "L", "N", "O", "Q", "R", "U", "V", "Y", "Z", "H", "D", "I", "W"}
+	deleteNodes := []string{"T"}
+
+	rootTree := &BTree{nil, 3}
+	rootTree.Create()
+
+	for _, node := range nodes {
+		rootTree.Insert(node)
+	}
+
+	for _, node := range deleteNodes {
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:4], []string{"P", "U", "X"}) {
+		t.Error("Error, expected: [P U X]")
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].C[3].Key[1:3], []string{"V", "W"}) {
+		t.Error("Error, expected: [V W]")
+	}
+}
+
+func TestDeleteP(t *testing.T) {
+	nodes := []string{"P", "C", "G", "T", "X", "A", "B", "E", "F", "J", "K", "L", "N", "O", "Q", "R", "U", "V", "Y", "Z", "H", "D", "I", "W"}
+	deleteNodes := []string{"P"}
+
+	rootTree := &BTree{nil, 3}
+	rootTree.Create()
+
+	for _, node := range nodes {
+		rootTree.Insert(node)
+	}
+
+	for _, node := range deleteNodes {
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:3], []string{"T", "X"}) {
+		t.Error("Error, expected: [T X]")
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].C[1].Key[1:5], []string{"N", "O", "Q", "R"}) {
+		t.Error("Error, expected: [N O Q R]")
+	}
+}
+
+func TestDeleteRecurse(t *testing.T) {
+	nodes := []string{"P", "C", "T", "G", "M", "X", "A", "B", "D", "Q", "R", "U", "V", "E", "F", "J", "K", "Y", "Z", "N", "O"}
+	deleteNodes := []string{"Z", "Y", "X"}
+
+	rootTree := &BTree{nil, 2}
+	rootTree.Create()
+
+	for _, node := range nodes {
+		rootTree.Insert(node)
+	}
+
+	deleteNodes = []string{"R", "Q"}
+	for _, node := range deleteNodes {
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:3], []string{"K", "O"}) {
+		t.Error("Error, expected: [K O]")
+	}
+
+	if !reflect.DeepEqual(rootTree.root.C[2].C[3].Key[1:2], []string{"P"}) {
+		t.Error("Error, expected: [P]")
+	}
+}
+
+func TestDeleteTurn(t *testing.T) {
+	nodes := []string{"A", "D", "F", "L", "B", "C", "E", "H", "J", "G", "I", "K", "N", "M", "Q", "R", "S", "T", "P", "X", "Y", "Z"}
+
+	rootTree := &BTree{nil, 3}
+	rootTree.Create()
+
+	for _, node := range nodes {
+		rootTree.Insert(node)
+	}
+
+	node := "Z"
+	rootTree.Delete(rootTree.root, node)
+	elem, _ := rootTree.Search(rootTree.root, node)
+	if elem != nil {
+		t.Errorf("Found deleted element: %s", node)
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:6], []string{"D", "H", "K", "N", "R"}) {
+		t.Error("Error, expected: [D H K N R]")
+	}
+
+	node = "R"
+	rootTree.Delete(rootTree.root, node)
+	elem, _ = rootTree.Search(rootTree.root, node)
+	if elem != nil {
+		t.Errorf("Found deleted element: %s", node)
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:6], []string{"D", "H", "K", "N", "S"}) {
+		t.Error("Error, expected: [D H K N S]")
+	}
+	if !reflect.DeepEqual(rootTree.root.C[6].Key[1:4], []string{"T", "X", "Y"}) {
+		t.Error("Error, expected: [T X Y]")
+	}
+
+	node = "H"
+	rootTree.Delete(rootTree.root, node)
+	elem, _ = rootTree.Search(rootTree.root, node)
+	if elem != nil {
+		t.Errorf("Found deleted element: %s", node)
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:6], []string{"D", "G", "K", "N", "S"}) {
+		t.Error("Error, expected: [D G K N S]")
+	}
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:3], []string{"E", "F"}) {
+		t.Error("Error, expected: [E F]")
+	}
+
+	node = "K"
+	rootTree.Delete(rootTree.root, node)
+	elem, _ = rootTree.Search(rootTree.root, node)
+	if elem != nil {
+		t.Errorf("Found deleted element: %s", node)
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:5], []string{"D", "G", "N", "S"}) {
+		t.Error("Error, expected: [D G N S]")
+	}
+	if !reflect.DeepEqual(rootTree.root.C[3].Key[1:5], []string{"I", "J", "L", "M"}) {
+		t.Error("Error, expected: [I J L M]")
+	}
+
+	node = "E"
+	rootTree.Delete(rootTree.root, node)
+	elem, _ = rootTree.Search(rootTree.root, node)
+	if elem != nil {
+		t.Errorf("Found deleted element: %s", node)
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:5], []string{"C", "G", "N", "S"}) {
+		t.Error("Error, expected: [D G N S]")
+	}
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:3], []string{"D", "F"}) {
+		t.Error("Error, expected: [D F]")
+	}
+
+	node = "F"
+	rootTree.Delete(rootTree.root, node)
+	elem, _ = rootTree.Search(rootTree.root, node)
+	if elem != nil {
+		t.Errorf("Found deleted element: %s", node)
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:5], []string{"C", "I", "N", "S"}) {
+		t.Error("Error, expected: [C I N S]")
+	}
+	if !reflect.DeepEqual(rootTree.root.C[3].Key[1:4], []string{"J", "L", "M"}) {
+		t.Error("Error, expected: [J L M]")
+	}
+
+	node = "D"
+	rootTree.Delete(rootTree.root, node)
+	elem, _ = rootTree.Search(rootTree.root, node)
+	if elem != nil {
+		t.Errorf("Found deleted element: %s", node)
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:5], []string{"C", "J", "N", "S"}) {
+		t.Error("Error, expected: [C J N S]")
+	}
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:3], []string{"G", "I"}) {
+		t.Error("Error, expected: [G I]")
+	}
+
+	node = "G"
+	rootTree.Delete(rootTree.root, node)
+	elem, _ = rootTree.Search(rootTree.root, node)
+	if elem != nil {
+		t.Errorf("Found deleted element: %s", node)
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:4], []string{"C", "N", "S"}) {
+		t.Error("Error, expected: [C N S]")
+	}
+	if !reflect.DeepEqual(rootTree.root.C[2].Key[1:5], []string{"I", "J", "L", "M"}) {
+		t.Error("Error, expected: [I J L M]")
+	}
+
+	node = "T"
+	rootTree.Delete(rootTree.root, node)
+	elem, _ = rootTree.Search(rootTree.root, node)
+	if elem != nil {
+		t.Errorf("Found deleted element: %s", node)
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:4], []string{"C", "N", "S"}) {
+		t.Error("Error, expected: [C N S]")
+	}
+	if !reflect.DeepEqual(rootTree.root.C[4].Key[1:3], []string{"X", "Y"}) {
+		t.Error("Error, expected: [X Y]")
+	}
+
+	node = "X"
+	rootTree.Delete(rootTree.root, node)
+	elem, _ = rootTree.Search(rootTree.root, node)
+	if elem != nil {
+		t.Errorf("Found deleted element: %s", node)
+	}
+
+	if !reflect.DeepEqual(rootTree.root.Key[1:3], []string{"C", "N"}) {
+		t.Error("Error, expected: [C N]")
+	}
+	if !reflect.DeepEqual(rootTree.root.C[3].Key[1:5], []string{"P", "Q", "S", "Y"}) {
+		t.Error("Error, expected: [P Q S Y]")
+	}
+}
+
+func TestDeleteLargeTree2(t *testing.T) {
+	rootTree := &BTree{nil, 2}
+	rootTree.Create()
+
+	for i := 1000; i < 10000; i++ {
+		rootTree.Insert(strconv.Itoa(i))
+	}
+
+	var node string
+	for i := 1000; i < 10000; i++ {
+		node = (strconv.Itoa(i))
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+}
+
+func TestDeleteLargeTree3(t *testing.T) {
+	rootTree := &BTree{nil, 3}
+	rootTree.Create()
+
+	for i := 1000; i < 10000; i++ {
+		rootTree.Insert(strconv.Itoa(i))
+	}
+
+	var node string
+	for i := 1000; i < 10000; i++ {
+		node = (strconv.Itoa(i))
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+}
+
+func TestDeleteLargeTree4(t *testing.T) {
+	rootTree := &BTree{nil, 4}
+	rootTree.Create()
+
+	for i := 1000; i < 10000; i++ {
+		rootTree.Insert(strconv.Itoa(i))
+	}
+
+	var node string
+	for i := 1000; i < 10000; i++ {
+		node = (strconv.Itoa(i))
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
+}
+
+func TestDeleteLargeTree5(t *testing.T) {
+	rootTree := &BTree{nil, 5}
+	rootTree.Create()
+
+	for i := 10000; i < 100000; i++ {
+		rootTree.Insert(strconv.Itoa(i))
+	}
+
+	var node string
+	for i := 10000; i < 100000; i++ {
+		node = (strconv.Itoa(i))
+		rootTree.Delete(rootTree.root, node)
+		elem, _ := rootTree.Search(rootTree.root, node)
+		if elem != nil {
+			t.Errorf("Found deleted element: %s", node)
+		}
+	}
 }
